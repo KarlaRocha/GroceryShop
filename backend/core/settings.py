1f6956514db47accd617d73906b086c6126b7102
Karla Rocha <leonoemi123@gmail.com>
2021-05-16 00:31:34 -0700

    updated schema settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -128,5 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 GRAPHENE = {
-    "SCHEMA": "grocery.schema.schema"
+    "SCHEMA": "schema.schema.schema"
 }
--- backend/schema/schema.py
+++ backend/schema/schema.py
@@ -14,31 +14,3 @@ class Query(ProductQuery, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query)
-
-
-# class CategoryType(DjangoObjectType):
-#     class Meta:
-#         model = Category
-#         fields = ("id", "name", "products")
-
-
-# class ProductType(DjangoObjectType):
-#     class Meta:
-#         model = Product
-#         fields = ("id", "name", "description", "category")
-
-
-# class Query(graphene.ObjectType):
-#     all_products = graphene.List(ProductType)
-#     category_by_name = graphene.Field(CategoryType, name=graphene.String(required=True))
-
-#     def resolve_all_products(root, info):
-#         return Product.objects.select_related("category").all()
-
-#     def resolve_category_by_name(root, info, name):
-#         try:
-#             return Category.objects.get(name=name)
-#         except Category.DoesNotExist:
-#             return None
-
-# schema = graphene.Schema(query=Query)
